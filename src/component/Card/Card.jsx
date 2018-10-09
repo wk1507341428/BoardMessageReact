22869ea9a9f6cc2042fbaa03207b2693574b3547
kangwang7 <wk1507341428@gmail.com>
2018-10-09 22:36:29 +0800

    鼠标移动事件完毕

--- src/component/Card/Card.jsx
+++ src/component/Card/Card.jsx
@@ -1,51 +1,139 @@
 import React from 'react'
 import './card.scss'
 
+let body = document.getElementsByTagName("body")[0]
+body.addEventListener("mouseup",function(e){
+    // 0表示左键
+    if(e.button!=0) return
+    window.flag = false
+})
+
 class Card extends React.Component {
   constructor(){
     super()
-    this.state={
-      x:"",
-      y:""
-    }
+    this.state={}
+  }
+  // 第一次渲染后调用，只在客户端，已经生成了对应得DOM结构
+  componentDidMount(){
+    let zIndex = window.zIndex
+    this.setState({zIndex})
   }
   // 钩子函数，当props传入该组件时触发
   componentWillReceiveProps(data){
+    // TODO
+    // 在这里触发Card的随机位置有弊端
+    // 如果有动态的props传入会刷新随机位置
     let position = data.position
     this.computeCardPosition(position)
   }
 
-  // 计算卡片随机位置
+  // 计算卡片随机位置,并且存入state中
   computeCardPosition=(position)=>{
     let cardWidth = (this.refs.card&&this.refs.card.offsetWidth)||0;
     let cardHeight = (this.refs.card&&this.refs.card.offsetHeight)||0;
     let x = parseInt(Math.random() * (position.width-cardWidth))
     let y = parseInt(Math.random() * (position.height-cardHeight))
+    this.setState({x,y,cardWidth,cardHeight})
+  }
+
+  // z-index 提升
+  handleClick = ()=>{
+    let zIndex = ++window.zIndex
+    this.setState({zIndex})
+  }
+
+  // 点击鼠标左键,开启阀门
+  handleMouseDown = (e)=>{
+    // 0表示左键
+    if(e.button!=0) return
+    this.handleClick()
+    window.flag = true
+  }
+
+  // // TODO
+  // // 松开鼠标左键，关闭阀门，这个应该在全局下
+  // handleMouseUp = (e)=>{
+  //   // 0表示左键
+  //   if(e.button!=0) return
+  //   window.flag = false
+  // }
+
+  // 跟随鼠标移动
+  handleMove = (e)=>{
+    let flag = window.flag
+    if(!flag) return
+    let props = this.props
+    let state = this.state
+    let cardWidthHalf = state.cardWidth * .5
+    let cardHeightHalf = state.cardHeight * .5
+    let x = e.pageX - props.position.offsetLeft - cardWidthHalf
+    let y = e.pageY - props.position.offsetTop - cardHeightHalf
+    // 边界限制
+    let boardWidth = props.position.width - cardWidthHalf*2
+    let boardHeight = props.position.height - cardHeightHalf*2
+    if(x<0)x=0
+    if(x>boardWidth)x=boardWidth
+    if(y<0)y=0
+    if(y>boardHeight)y=boardHeight
     this.setState({x,y})
   }
 
+  // 常规dom的方式
+  // mousedown 时绑定mouseover
+  // mouseup 时解绑mouseover
+  // 但是有没有react的思路呢
+  // 思路一
+  // mousedown时，设置一个阀门，通过这个值开启鼠标移动
+  // mouseup时，关闭阀门
+  // TODO 
+  // 这个阀门是不是应该是全局的
+
+
   render() {
+    // 在state中获取随机位置和z-index
+    let state = this.state
     let position = {
-      left:this.state.x,
-      top:this.state.y
+      left: state.x,
+      top: state.y,
+      zIndex: state.zIndex
     }
+    // 获取父元素传入的data
+    let data = this.props.data
+    // 这里感觉有问题，我想终止渲染，这样做也能达成目的,但是总感觉应该有API
+    if(JSON.stringify(data)==="{}") {
+      return(<div></div>)
+    }
+
     return (
-      <div ref="card" style={position} className="card">
+      <div ref="card" onClick={this.handleClick} 
+        onMouseMove={this.handleMove}
+        onMouseDown={this.handleMouseDown}
+        // onMouseUp={this.handleMouseUp}
+        style={position} className="card">
         <div className="card_h">
-          <div className="num">第[9]条 2016-02-22 01:30:00</div>
+          <div className="num">第[{data.id}]条 {data.time}</div>
           <div className="close" title="关闭纸条">×</div>
         </div>
         <div className="card_c">
-          班主任:“小明，你都十二岁了，还是三年级，不觉得羞愧吗”？。小明:“一点也不觉得，老师你都四十多岁了，不也是年年在三年级混日子吗？羞愧的应该是你”。老师:……
+          {data.content}
         </div>
         <div className="card_f">
           <div className="icon">
             <img src={require('../../assets/images/bpic_1.gif')} />
           </div>
-          <div className="name">没猴哥，不春晚</div>
+          <div className="name">{data.name}</div>
         </div>
       </div>
     )
+  }
+}
+
+// 设置props默认值
+Card.defaultProps={
+  data:{},
+  position:{
+    width:960,
+    heightL:562
   }
 }
 
--- src/views/MsgBoard/MsgBoard.jsx
+++ src/views/MsgBoard/MsgBoard.jsx
@@ -1,43 +1,79 @@
 import React from 'react'
 import './MsgBoard.scss'
-import Card from'../../component/Card/Card'
+import Card from '../../component/Card/Card'
 
-var x = parseInt(Math.random() * 700);
-var y = parseInt(Math.random() * 300);
+window.zIndex = 1
+window.flag = false
 
 class ShowMsg extends React.Component {
 
-  constructor(){
+  constructor() {
     super()
-    this.state={
-      boardWidth:"",
-      boardHeight:""
+    this.state = {
+      boardWidth: "",
+      boardHeight: "",
+      datas: [
+        { "id": 1, "name": "mahu", "content": "今天你拿苹果支付了么", "time": "2016-02-17 00:00:00" },
+        { "id": 2, "name": "haha", "content": "今天天气不错，风和日丽的", "time": "2016-02-18 12:40:00" },
+        { "id": 3, "name": "jjjj", "content": "常要说的事儿是乐生于苦", "time": "2016-03-18 12:40:00" },
+        { "id": 4, "name": "9.8的妹纸", "content": "把朋友家厕所拉堵了 不敢出去 掏了半小时了都", "time": "2016-03-18 12:40:00" },
+        { "id": 5, "name": "雷锋ii.", "content": "元宵节快乐", "time": "2016-02-22 12:40:00" },
+        { "id": 6, "name": "哎呦哥哥.", "content": "据说今晚央视的元宵晚会导演和春晚导演是同一个人，真是躲得过初一，躲不过十五。", "time": "2016-02-22 01:30:00" },
+        { "id": 7, "name": "哎呦杰杰.", "content": "真搞不懂你们地球人，月亮有什么好看的，全是坑，还是对面那哥们好看，", "time": "2016-02-22 01:30:00" },
+        { "id": 8, "name": "哎呦哎呦", "content": "今天哪里的烟花最好看！！？答：朋友圈。。。", "time": "2016-02-22 02:30:00" },
+        {
+          "id": 9,
+          "name": "没猴哥，不春晚",
+          "content": "班主任:“小明，你都十二岁了，还是三年级，不觉得羞愧吗”？。小明:“一点也不觉得，老师你都四十多岁了，不也是年年在三年级混日子吗？羞愧的应该是你”。老师:……",
+          "time": "2016-02-22 01:30:00"
+        }
+      ]
     }
   }
 
-  componentDidMount(){
+  // 钩子函数
+  componentDidMount() {
     this.getBoardInfo()
   }
 
   // 获取留言板容器宽和高并且存入state中
-  getBoardInfo = ()=>{
-    let boardWidth = this.refs.messageBoard.offsetWidth
-    let boardHeight = this.refs.messageBoard.offsetHeight
+  getBoardInfo = () => {
+    let messageBoard = this.refs.messageBoard
+    let boardWidth = messageBoard.offsetWidth
+    let boardHeight = messageBoard.offsetHeight
+    let offsetLeft = messageBoard.offsetLeft
+    let offsetTop = messageBoard.offsetTop
     this.setState({
       boardWidth,
-      boardHeight
+      boardHeight,
+      offsetLeft,
+      offsetTop
     })
   }
 
+  // 每次setState的时候都会触发render函数一次，这样会不会影响性能？
   render() {
+    let state = this.state
     // 将board的信息传入子组件，计算随机位置时候会用到
     let boardInfo = {
-      width:this.state.boardWidth,
-      height:this.state.boardHeight
+      width: state.boardWidth,
+      height: state.boardHeight,
+      offsetLeft: state.offsetLeft,
+      offsetTop: state.offsetTop
     }
+
+    // 动态渲染card
+    let datas = this.state.datas||[]
+    const CardList = datas.map((item,i)=>{
+      return(
+        <Card
+          key={i} position={boardInfo} 
+          data={item}/>
+      )
+    })
     return (
       <div ref="messageBoard" className="messageBoard">
-        <Card position={boardInfo}/>
+        {CardList}
       </div>
     )
   }
